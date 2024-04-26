2e9434ce94f20ece469934efd104275d2f0733f4
maria.lemos.de.souza <maria.lemos.de.souza@WINAPD0IMCYETZ0>
2024-04-26 15:48:58 -0300

    Adicionar arquivos de projeto.

---
+++ ImpactaCursoAluno/Controllers/CursoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImpactaCursoAluno.Controllers
+{
+    public class CursoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
